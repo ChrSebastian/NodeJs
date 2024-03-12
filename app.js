e3f7b51cdd60f9d8978d58be97210c273e50b6c4
ChrSebastian <chirvaisebastian@yahoo.com>
2024-03-12 18:55:34 +0200

    made 'path' variable available globally

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Middleware to set 'path' variable globally available in all views
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
