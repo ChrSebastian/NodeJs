468fce2482b3058f9f0c4b1bfac424ce125e762b
ChrSebastian <chirvaisebastian@yahoo.com>
2024-03-12 16:37:36 +0200

    switched the template engine from pub to handlebars, created hbs files for it and modified the render functions

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine("hbs", expressHbs.engine({ defaultLayout: null }));
+app.set("view engine", "hbs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -19,7 +21,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { title: "Page not Found" });
+  res.status(404).render("404", { pageTitle: "Page not Found" });
 });
 
 app.listen(3000);
