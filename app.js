6fd57fdbd24857422732a402f7b7a11d733c9848
ChrSebastian <chirvaisebastian@yahoo.com>
2024-03-13 18:52:07 +0200

    added separate file controller for 404

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const errorController = require("./controllers/error");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -23,8 +25,6 @@ app.use((req, res, next) => {
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
