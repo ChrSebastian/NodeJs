46a45d922c3bad2b980506bda9631a11dda1aca8
ChrSebastian <chirvaisebastian@yahoo.com>
2024-03-26 17:04:14 +0200

    Finishing the Database Connection

--- util/database.js
+++ util/database.js
@@ -4,13 +4,27 @@ const MongoClient = mongodb.MongoClient;
 const url =
   "mongodb+srv://Bastyze98:Bastyze98@cluster0.qg5czrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(url)
     .then((client) => {
       console.log("Connected!");
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
