f11090669f771d5d0b881e9437c48841544f9398
ChrSebastian <chirvaisebastian@yahoo.com>
2024-03-26 15:23:09 +0200

    successfully connected to MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,16 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "nodecomplete", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const url =
+  "mongodb+srv://Bastyze98:Bastyze98@cluster0.qg5czrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(url)
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = mongoConnect;
